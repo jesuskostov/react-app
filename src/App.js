2cc2233948ef955beb0ded8f44dd3857450a6a39
jesuskostov <48697426+jesuskostov@users.noreply.github.com>
2022-09-28 14:06:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Heyyy</p>
+        <p>Heyyy 2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
